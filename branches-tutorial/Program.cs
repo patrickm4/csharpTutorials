82799adef9b2ed7da2f0bf5ce1bef9b0ee4f1368
unknown <patrickmoreno444@gmail.com>
2023-02-09 22:58:48 -0800

    master start Branches tut and wip List tut

--- branches-tutorial/Program.cs
+++ branches-tutorial/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//ExploreIf();
+
+void ExploreIf()
+{
+    int a = 5;
+    int b = 3;
+    if (a + b > 10)
+    {
+        Console.WriteLine("The answer is greater than 10");
+    }
+    else
+    {
+        Console.WriteLine("The answer is not greater than 10");
+    }
+
+    int c = 4;
+    if ((a + b + c > 10) && (a > b))
+    {
+        Console.WriteLine("The answer is greater than 10");
+        Console.WriteLine("And the first number is greater than the second");
+    }
+    else
+    {
+        Console.WriteLine("The answer is not greater than 10");
+        Console.WriteLine("Or the first number is not greater than the second");
+    }
+
+    if ((a + b + c > 10) || (a > b))
+    {
+        Console.WriteLine("The answer is greater than 10");
+        Console.WriteLine("Or the first number is greater than the second");
+    }
+    else
+    {
+        Console.WriteLine("The answer is not greater than 10");
+        Console.WriteLine("And the first number is not greater than the second");
+    }
+}
---
+++ list-tutorial/Program.cs
@@ -0,0 +1,19 @@
+﻿var names = new List<string> {"Pat", "Ana", "Felipe"};
+
+foreach (var name in names) {
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
+
+Console.WriteLine();
+names.Add("Maria");
+names.Add("Bill");
+names.Remove("Ana");
+
+foreach (var name in names) {
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
+
+Console.WriteLine($"My name is {names[0]}");
+Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+
+Console.WriteLine($"The list has {names.Count} people in it");

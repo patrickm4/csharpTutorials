f800deac6ac834548335981809883e6634f96b86
Patrick Moreno <patrickmoreno444@gmail.com>
2023-02-12 16:30:04 -0800

    master classes tutorial

---
+++ classes/Program.cs
@@ -0,0 +1,31 @@
+﻿using Classes;
+
+var account = new BankAccount("Pat", 1000);
+
+account.MakeWIthdrawal(500, DateTime.Now, "Rent payment");
+Console.WriteLine(account.Balance);
+account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+Console.WriteLine(account.Balance);
+
+// test for negative balance
+try {
+    account.MakeWIthdrawal(750, DateTime.Now, "Attempt to overdraw");
+}
+catch (InvalidOperationException e) {
+    Console.WriteLine("Exception caught trying to overdraw");
+    Console.WriteLine(e.ToString());
+}
+
+// test initial balance is positive
+BankAccount invalidAccount;
+try {
+    invalidAccount = new BankAccount("invalid", -55);
+}
+catch (ArgumentOutOfRangeException e) {
+    Console.WriteLine("Exception caught creating account with negative balance");
+    Console.WriteLine(e.ToString());
+}
+
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+Console.WriteLine(account.GetAccountHistory());
